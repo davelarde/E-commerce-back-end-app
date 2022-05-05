b39f5e7cc834d708916142962e8ae42b07a7cbc4
danivelarde <danielavelarde4@gmail.com>
2022-05-05 17:34:38 -0400

    routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,6 +6,14 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  await Tag.findAll({
+    attributes: ["id", "tag_name"],
+    include:[{
+      model: Product,
+      attributes:["id", "product_name", "price", "stock", "category_id"],
+      through: ProductTag,
+    },],
+  })
 });
 
 router.get('/:id', (req, res) => {
